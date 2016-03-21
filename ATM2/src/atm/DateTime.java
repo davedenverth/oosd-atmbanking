eb5b27ef325818eeab95e77c911def16b2b1f158
davedenverth <suphakon_game@hotmail.com>
2016-03-21 20:00:22 +0700

    I added a new class that can use to call for date and time by reducing code duplication.

---
+++ ATM2/src/atm/DateTime.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+/**
+ *
+ * @author Game
+ */
+public class DateTime {
+    private final String formatDate = "dd.MM.yyyy";
+    private final String formatTime = "hh:mm:ss";
+    String realFormat;
+    
+    public DateTime(String format){
+        Date dateTime = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat(format);
+        realFormat = String.valueOf(ft.format(dateTime));
+    }
+}
