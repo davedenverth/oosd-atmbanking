435a54ad2086fe8c1892ade49f1ed72ee4f88049
davedenverth <suphakon_game@hotmail.com>
2016-05-02 16:07:53 +0700

    Change db variable to be inside the superclass Popup in public that can make a call by all subclasses.

--- ATM2/src/atm/PopUp.java
+++ ATM2/src/atm/PopUp.java
@@ -10,6 +10,9 @@ import javax.swing.JFrame;
  * @author Game
  */
 public abstract class PopUp extends JFrame{
+    
+    public ConnectDB db;
+    
     public boolean isNumeric(String s){
         try{
             Integer.parseInt(s);
