b70be15e099ee821cab1b3a289cd17e8db5857f1
davedenverth <suphakon_game@hotmail.com>
2016-05-02 16:05:14 +0700

    Add a functionATM interface to implement by main function classes.

---
+++ ATM2/src/atm/FunctionATM.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+import java.io.FileNotFoundException;
+/**
+ *
+ * @author Game
+ */
+public interface FunctionATM {
+    public void performFunction() throws FileNotFoundException;
+}
